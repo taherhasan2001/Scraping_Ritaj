81eeeb39552fe9a629f462233f9a9116a79f13bc
taher hasan <112001968+TaherHasan94@users.noreply.github.com>
2024-02-27 12:28:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,10 +25,75 @@ courses_names = ["PUBA","ANTH",
 "PASP","TOUR","TRAN","WOMS",
 
 ]
+url = ''
 
+def openEdge():
+    pyautogui.press('win')
+    time.sleep(0.1)
+
+    pyautogui.typewrite('Microsoft Edge')
+
+    # Add a small delay to ensure the typing is completed before pressing Enter
+    time.sleep(0.1)
+
+    # Press Enter
+    pyautogui.press('enter')
+    time.sleep(0.1)
+
+    pyautogui.hotkey('ctrl', 'l')
+    time.sleep(0.1)
+    pyautogui.typewrite(url)
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(1)
+    pyautogui.press('f11')
+    time.sleep(0.1)
 def write_to_json():
     with open(f'BZU.json','w', encoding='utf-8') as json_file:
         json.dump(sections, json_file, ensure_ascii=False, indent=4)
+
+def get_data(delay=5):
+
+    # Adjust the sleep time as needed to allow the page to fully load
+    time.sleep(delay)
+    # Get the screen width and height
+    screen_width, screen_height = pyautogui.size()
+
+    # Calculate the coordinates for the middle of the screen
+    middle_x = screen_width // 2
+    middle_y = screen_height // 2
+
+    # Move the mouse to the middle of the screen
+    pyautogui.moveTo(middle_x, middle_y)
+
+    # Perform a mouse click (left-click) at the current mouse position (middle of the screen)
+    pyautogui.click()
+
+
+    # Use PyAutoGUI to simulate keyboard shortcuts for copying
+    pyautogui.hotkey('ctrl', 'a')  # Select all
+    pyautogui.hotkey('ctrl', 'c')  # Copy
+
+    # Close the current tab (You need to implement this function)
+    # close_current_tab()
+
+    # Add a delay before attempting to paste from clipboard
+    time.sleep(delay)
+
+    # Retrieve the copied text from the clipboard
+    copied_text = clipboard.paste()
+
+    # Print the copied text (for debugging)
+
+    # Add another delay before writing to a file
+    time.sleep(delay)
+
+    # Save the copied text to a file
+    with open("BZU.txt", "w", encoding="utf-8") as file:
+        file.write(copied_text)
+
+    read_from_txt()
+    close_current_tab()
 
 
 def read_from_txt():
@@ -44,6 +109,7 @@ def read_from_txt():
     with open(f'BZU.txt','r', encoding='utf-8') as file:
         lines = file.readlines()
         read_course_name = False
+        isLab = False
         subCourseName = ''
         for line in lines:
             line = line.strip()
@@ -77,7 +143,7 @@ def read_from_txt():
             # Check if the line contains section information
 
             if line.split('\t')[0] == 'Lab':
-                subCourseName = True
+                isLab = True
                 current_section['sec'] = int(line.split('\t')[1])
 
                 current_section['name of instructor'] = line.split('\t')[2]
@@ -94,6 +160,7 @@ def read_from_txt():
                 continue
             if line.split('\t')[0] == 'Lecture':
                 subCourseName = False
+                isLab = False
                 current_section['sec'] = int(line.split('\t')[1])
 
                 current_section['name of instructor'] = line.split('\t')[2]
@@ -112,7 +179,10 @@ def read_from_txt():
                     sections.append(current_section.copy())
                 if not flagError:
                     current_section['place'] = line.split('\t')[2]
-                    if subCourseName:
+                    if isLab:
+                        current_section['name of course'] = 'lab' + current_section['name of course'].replace('lab', '')
+
+                    elif subCourseName:
                         current_section['name of course'] = 'sub'+current_section['name of course'].replace('sub', '')
                     sections.append(current_section.copy())
 
@@ -126,50 +196,6 @@ def close_current_tab():
 
 
 
-def get_data(url, delay=5):
-    # Open the URL in a web browser
-    webbrowser.open(url)
-
-    # Adjust the sleep time as needed to allow the page to fully load
-    time.sleep(delay)
-    # Get the screen width and height
-    screen_width, screen_height = pyautogui.size()
-
-    # Calculate the coordinates for the middle of the screen
-    middle_x = screen_width // 2
-    middle_y = screen_height // 2
-
-    # Move the mouse to the middle of the screen
-    pyautogui.moveTo(middle_x, middle_y)
-
-    # Perform a mouse click (left-click) at the current mouse position (middle of the screen)
-    pyautogui.click()
-
-
-    # Use PyAutoGUI to simulate keyboard shortcuts for copying
-    pyautogui.hotkey('ctrl', 'a')  # Select all
-    pyautogui.hotkey('ctrl', 'c')  # Copy
-
-    # Close the current tab (You need to implement this function)
-    # close_current_tab()
-
-    # Add a delay before attempting to paste from clipboard
-    time.sleep(10)
-
-    # Retrieve the copied text from the clipboard
-    copied_text = clipboard.paste()
-
-    # Print the copied text (for debugging)
-
-    # Add another delay before writing to a file
-    time.sleep(3)
-
-    # Save the copied text to a file
-    with open("BZU.txt", "w", encoding="utf-8") as file:
-        file.write(copied_text)
-
-    read_from_txt()
-    close_current_tab()
 
 # function that read from file "link_here.txt" and read the first line as link
 def read_link():
@@ -185,11 +211,20 @@ def remove_file(filename):
         print(f"{filename} ERROR with removing this file 'contact with Taher Hasan'.")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-
+    # "date": "2024-01-20",
+    # "time": "11:45:00"
 if __name__ == '__main__':
+    url = read_link()
+    openEdge()
+    dateNow = time.strftime('%Y-%m-%d')
+    timeNow = time.strftime('%H:%M:%S')
 
-    get_data( read_link())
+    get_data(delay= 1)
 
     write_to_json()
     remove_file("BZU.txt")
-    input(f"Number of sections: {len(sections)}\nPress Enter to exit...")
+    # write the time and date in date.json in form : {"date": dateNow,"time": timeNow}
+    with open("date.json", "w") as file:
+        file.write(f'{{"date": "{dateNow}","time": "{timeNow}"}}')
+
+    # input(f"Number of sections: {len(sections)}\nPress Enter to exit...")
